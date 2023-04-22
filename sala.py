b6df34d3b4d6f80077d8b48965642313c6949107
edudmatos <edumatos96@outlook.com>
2023-04-22 17:03:16 -0300

    classe sala - novo arquivo

---
+++ sala.py
@@ -0,0 +1,20 @@
+class sala:
+    def __init__(self, numero: int, capacidade: int, tipo: str):
+        self._numero = numero
+        self._capacidade = capacidade
+        self._tipo = tipo
+
+    def get_numero(self) -> int:
+        return self._numero
+    def set_numero(self, numero: int):
+        self._numero = numero
+
+    def get_capacidade(self) -> int:
+        return self._capacidade
+    def set_capacidade(self, capacidade: int):
+        self._capacidade = capacidade
+
+    def get_tipo(self) -> str:
+        return self._tipo
+    def set_tipo(self, tipo: str):
+        self._tipo = tipo
