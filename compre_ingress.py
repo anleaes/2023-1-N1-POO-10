9242a511f479f3fa047865fb75dc5493a4f0c74d
Joao1909 <paulopereirajoao98@gmail.com>
2023-04-24 18:19:30 -0300

    Update .gitignore criação da branch compre ingresso

---
+++ compre_ingress.py
@@ -0,0 +1,41 @@
+from sessao import Sessao
+
+class compra_ingresso:
+    def _init_(self, forma_pagamento: int, preco: float, assento: int, TPingresso: int, sessao: Sessao):
+
+        self.__forma_pagamento = forma_pagamento
+        self.__preco = preco
+        self.__TPingresso = TPingresso
+        self.__assento = assento
+        self.__sessao = sessao
+       
+
+    def get_forma_pagamento(self) -> int:
+        return self.__forma_pagamento
+
+    def set_forma_pagamento(self, forma_pagamento: int):
+        self.__forma_pagamento = forma_pagamento
+
+    def get_assento(self) -> int:
+        return self.__assento
+
+    def set_assento(self, assento: int):
+        self.__assento = assento
+
+    def get_TPingresso(self) -> int:
+        return self.__TPingresso
+
+    def set_TPingresso(self, TPingresso: int):
+        self.__TPingresso = TPingresso
+        
+    def get_preco(self) -> float:
+        return self.__preco
+
+    def set_preco(self, preco: float):
+        self.__preco = preco
+
+    def get_sessao(self) -> Sessao:
+        return self.__sessao
+
+    def set_sessao(self, sessao: Sessao):
+        self.__sessao = sessao
